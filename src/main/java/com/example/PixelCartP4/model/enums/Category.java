194d0e447a3ef481e3e6448c6bc60bbc88149242
Smit Kadam <smitkadam148@gmail.com>
2026-02-11 12:33:15 +0530

    1

---
+++ src/main/java/com/example/PixelCartP4/model/enums/Category.java
@@ -0,0 +1,5 @@
+package com.example.PixelCartP4.model.enums;
+
+public enum Category {
+    PCGames, ConsoleGames, Gear, 
+}
---
+++ src/main/java/com/example/PixelCartP4/model/enums/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.example.PixelCartP4.model.enums;
+
+public enum OrderStatus {
+    PENDING, PROCESSING, COMPLETED, CANCELLED
+}
